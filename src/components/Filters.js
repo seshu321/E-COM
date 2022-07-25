8b7dbe805d02a21f833cefaea03618e1c67254b7
seshu321 <kotkumar2@publicisgroupe.net>
2022-07-25 12:18:48 +0530

    testjsjs

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -13,9 +13,12 @@ const Wrapper = styled.main`
     width:80%;
  
 }
+
   .sort {
     margin-top:2rem;
     select {
+      background: var(--clr-grey-10);
+      border:none;
         width:80%;
       padding: 4px;
       font-size: 1rem;
@@ -26,6 +29,8 @@ const Wrapper = styled.main`
     margin:2rem 0;
     font-size:1rem;
     select {
+      background: var(--clr-grey-10);
+      border:none;;
         width:80%;
         padding: 4px;
         font-size: 1rem;
@@ -65,6 +70,101 @@ const Wrapper = styled.main`
     border-radius:5px;
     border:none;
   }
+  .price-container{
+      p{
+        margin:0 0;
+      }
+      input[type=range] {
+        height: 25px;
+        -webkit-appearance: none;
+        margin: 10px 0;
+        width: 80%;
+      }
+      input[type=range]:focus {
+        outline: none;
+      }
+      input[type=range]::-webkit-slider-runnable-track {
+        width: 80%;
+        height: 5px;
+        cursor: pointer;
+        animate: 0.2s;
+        box-shadow: 0px 0px 0px #000000;
+        background: #2497E3;
+        border-radius: 1px;
+        border: 0px solid #000000;
+      }
+      input[type=range]::-webkit-slider-thumb {
+        box-shadow: 0px 0px 0px #000000;
+        border: 1px solid #2497E3;
+        height: 18px;
+        width: 18px;
+        border-radius: 25px;
+        background: #A1D0FF;
+        cursor: pointer;
+        -webkit-appearance: none;
+        margin-top: -7px;
+      }
+      input[type=range]:focus::-webkit-slider-runnable-track {
+        background: #2497E3;
+      }
+      input[type=range]::-moz-range-track {
+        width: 80%;
+        height: 5px;
+        cursor: pointer;
+        animate: 0.2s;
+        box-shadow: 0px 0px 0px #000000;
+        background: #2497E3;
+        border-radius: 1px;
+        border: 0px solid #000000;
+      }
+      input[type=range]::-moz-range-thumb {
+        box-shadow: 0px 0px 0px #000000;
+        border: 1px solid #2497E3;
+        height: 18px;
+        width: 18px;
+        border-radius: 25px;
+        background: #A1D0FF;
+        cursor: pointer;
+      }
+      input[type=range]::-ms-track {
+        width: 80%;
+        height: 5px;
+        cursor: pointer;
+        animate: 0.2s;
+        background: transparent;
+        border-color: transparent;
+        color: transparent;
+      }
+      input[type=range]::-ms-fill-lower {
+        background: #2497E3;
+        border: 0px solid #000000;
+        border-radius: 2px;
+        box-shadow: 0px 0px 0px #000000;
+      }
+      input[type=range]::-ms-fill-upper {
+        background: #2497E3;
+        border: 0px solid #000000;
+        border-radius: 2px;
+        box-shadow: 0px 0px 0px #000000;
+      }
+      input[type=range]::-ms-thumb {
+        margin-top: 1px;
+        box-shadow: 0px 0px 0px #000000;
+        border: 1px solid #2497E3;
+        height: 18px;
+        width: 18px;
+        border-radius: 25px;
+        background: #A1D0FF;
+        cursor: pointer;
+      }
+      input[type=range]:focus::-ms-fill-lower {
+        background: #2497E3;
+      }
+      input[type=range]:focus::-ms-fill-upper {
+        background: #2497E3;
+      }
+      
+  }
   @media (max-width:992px){
     display:grid;
     grid-template-columns:1fr 1fr;
@@ -97,7 +197,6 @@ const Filters = () => {
 
   let categories = getUnique(allProducts, "category");
   let companies = getUnique(allProducts, "company");
-
 
   return (
     <Wrapper>
@@ -134,19 +233,19 @@ const Filters = () => {
             ))}
           </select>
         </div>
-        <div> 
-        <h5>price</h5>
-
-        <p className="price">{formatPrice(price)}</p>
-        <input
-          type="range"
-          name="price"
-          min={min_price}
-          max={max_price}
-          onChange={updateFilter}
-          value={price}
-        />
-      </div>
+        <div className="price-container">
+          <h5>price</h5>
+          <p className="price">{formatPrice(price)}</p>
+          <input
+            type="range"
+            name="price"
+            className="price-input"
+            min={min_price}
+            max={max_price}
+            onChange={updateFilter}
+            value={price}
+          />
+        </div>
       </div>
       <div className="right">
         <div className="categories">
@@ -164,12 +263,11 @@ const Filters = () => {
             ))}
           </div>
         </div>
-    
+
         <div className="free-shipping">
           <p>Free-shipping </p>
           <input
-          checked={shipping}
-            
+            checked={shipping}
             name="shipping"
             onChange={updateFilter}
             type="checkbox"
